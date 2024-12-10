c181961931c87878cc0387863da8cb573e559a52
Tafzeel Ahmed Khan <159202541+Tafzeel99@users.noreply.github.com>
2024-12-10 05:08:40 +0500

    first commit

--- src/app/order-completed/page.tsx
+++ src/app/order-completed/page.tsx
@@ -15,7 +15,7 @@ const OrderCompleted = () => {
                     </div>
                     <h1 className='font-josefin-sans text-4xl font-bold text-[#151875] capitalize text-center'>Your order is completed!</h1>
                     <p className='text-[#8D92A7] font-lato text-center'>Thank you for your order! Your order is being processed and will be completed within 3-6
-                        hours. You will receive an email confirmation when your order is completed.
+                        hours. You will receive an email confirmation, when your order is completed.
                     </p>
                     <button className='bg-[#fb2e86] rounded-md font-josefin-sans px-8 text-white py-3'>Continue Shipping</button>
                 </div>
