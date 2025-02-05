ca33f42a67b87bbb77fa4d417dc56869a64e1bba
Tafzeel Ahmed Khan <roshan.king2244@gmail.com>
2025-02-05 05:45:52 +0500

    Schema update

---
+++ types/products.ts
@@ -0,0 +1,23 @@
+
+
+
+export interface Product {
+    _id : string,
+    name : string,
+    _type : "product",
+    image? : {
+        asset : {
+            _ref : string,
+            _type: "image",
+
+        }
+    };
+    price : number,
+    description?: string,
+    discountPercentage: number,
+    isFeaturedProduct: Boolean,
+    stockLevel: number,
+    category: string,
+
+    
+}
